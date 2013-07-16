6b0b53ba6c9c36a319a31d690762aa9eabb27f31
Ange Albertini <ange.albertini@gmail.com>
2013-07-16 18:26:30 +0000

    PE with SafeSEH PE with SafeSEH, set on the fly
    git-svn-id: http://corkami.googlecode.com/svn/trunk@1660 0cc4791d-d43c-0410-a93a-b1ce904c0fb3

---
+++ safeseh.asm
@@ -0,0 +1,142 @@
+; a PE with SafeSEH (it should crash if the handler is not enabled in the list)
+
+; Ange Albertini, BSD LICENCE 2013
+
+%include 'consts.inc'
+
+IMAGEBASE equ 400000h
+org IMAGEBASE
+bits 32
+
+SECTIONALIGN equ 1000h
+FILEALIGN equ 200h
+
+istruc IMAGE_DOS_HEADER
+    at IMAGE_DOS_HEADER.e_magic, db 'MZ'
+    at IMAGE_DOS_HEADER.e_lfanew, dd NT_Signature - IMAGEBASE
+iend
+
+NT_Signature:
+istruc IMAGE_NT_HEADERS
+    at IMAGE_NT_HEADERS.Signature, db 'PE', 0, 0
+iend
+istruc IMAGE_FILE_HEADER
+    at IMAGE_FILE_HEADER.Machine,               dw IMAGE_FILE_MACHINE_I386
+    at IMAGE_FILE_HEADER.NumberOfSections,      dw NUMBEROFSECTIONS
+    at IMAGE_FILE_HEADER.SizeOfOptionalHeader,  dw SIZEOFOPTIONALHEADER
+    at IMAGE_FILE_HEADER.Characteristics,       dw IMAGE_FILE_EXECUTABLE_IMAGE | IMAGE_FILE_32BIT_MACHINE
+iend
+
+OptionalHeader:
+istruc IMAGE_OPTIONAL_HEADER32
+    at IMAGE_OPTIONAL_HEADER32.Magic,                     dw IMAGE_NT_OPTIONAL_HDR32_MAGIC
+    at IMAGE_OPTIONAL_HEADER32.AddressOfEntryPoint,       dd EntryPoint - IMAGEBASE
+    at IMAGE_OPTIONAL_HEADER32.ImageBase,                 dd IMAGEBASE
+    at IMAGE_OPTIONAL_HEADER32.SectionAlignment,          dd SECTIONALIGN
+    at IMAGE_OPTIONAL_HEADER32.FileAlignment,             dd FILEALIGN
+    at IMAGE_OPTIONAL_HEADER32.MajorSubsystemVersion,     dw 4
+    at IMAGE_OPTIONAL_HEADER32.SizeOfImage,               dd 2 * SECTIONALIGN
+    at IMAGE_OPTIONAL_HEADER32.SizeOfHeaders,             dd SIZEOFHEADERS
+    at IMAGE_OPTIONAL_HEADER32.Subsystem,                 dw IMAGE_SUBSYSTEM_WINDOWS_CUI
+    at IMAGE_OPTIONAL_HEADER32.NumberOfRvaAndSizes,       dd 16
+iend
+
+istruc IMAGE_DATA_DIRECTORY_16
+    at IMAGE_DATA_DIRECTORY_16.ImportsVA, dd Import_Descriptor - IMAGEBASE
+    at IMAGE_DATA_DIRECTORY_16.Load,      dd LoadConfig - IMAGEBASE, 40h ; fixed XP value?
+iend
+
+SIZEOFOPTIONALHEADER equ $ - OptionalHeader
+SectionHeader:
+istruc IMAGE_SECTION_HEADER
+    at IMAGE_SECTION_HEADER.VirtualSize,      dd 1 * SECTIONALIGN
+    at IMAGE_SECTION_HEADER.VirtualAddress,   dd 1 * SECTIONALIGN
+    at IMAGE_SECTION_HEADER.SizeOfRawData,    dd 1 * FILEALIGN
+    at IMAGE_SECTION_HEADER.PointerToRawData, dd 1 * FILEALIGN
+    at IMAGE_SECTION_HEADER.Characteristics,  dd IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_WRITE
+iend
+NUMBEROFSECTIONS equ ($ - SectionHeader) / IMAGE_SECTION_HEADER_size
+SIZEOFHEADERS equ $ - IMAGEBASE
+
+section progbits vstart=IMAGEBASE + SECTIONALIGN align=FILEALIGN
+
+;*******************************************************************************
+
+Handler:
+    push Msg
+    call [__imp__printf]
+    add esp, 1 * 4
+
+    push 42
+    call [__imp__ExitProcess]
+_c
+
+EntryPoint:
+    push Handler ; set an exception handler
+    push dword [fs:0]
+    mov [fs:0], esp
+
+    int3        ; trigger an exception
+_c
+
+Msg db "SafeSEH test: Exception handler called", 0ah, 0
+_d
+
+LoadConfig: ;*******************************************************************
+
+istruc IMAGE_LOAD_CONFIG_DIRECTORY32
+    at IMAGE_LOAD_CONFIG_DIRECTORY32.Size,           dd IMAGE_LOAD_CONFIG_DIRECTORY32_size
+    at IMAGE_LOAD_CONFIG_DIRECTORY32.SecurityCookie, dd cookie
+    at IMAGE_LOAD_CONFIG_DIRECTORY32.SEHandlerTable, dd HandlerTable
+    at IMAGE_LOAD_CONFIG_DIRECTORY32.SEHandlerCount, dd HANDLERCOUNT
+iend
+
+LOADCONFIGSIZE equ $ - LoadConfig
+
+cookie dd 0
+
+HandlerTable:
+    dd 1
+    ;dd Handler - IMAGEBASE ; add this to get the handler accepted
+    HANDLERCOUNT equ ($ - HandlerTable) / 4
+    dd 0
+
+;*******************************************************************************
+
+Import_Descriptor:
+istruc IMAGE_IMPORT_DESCRIPTOR
+    at IMAGE_IMPORT_DESCRIPTOR.OriginalFirstThunk, dd kernel32.dll_hintnames - IMAGEBASE
+    at IMAGE_IMPORT_DESCRIPTOR.Name1,              dd kernel32.dll - IMAGEBASE
+    at IMAGE_IMPORT_DESCRIPTOR.FirstThunk,         dd kernel32.dll_iat - IMAGEBASE
+iend
+istruc IMAGE_IMPORT_DESCRIPTOR
+    at IMAGE_IMPORT_DESCRIPTOR.OriginalFirstThunk, dd msvcrt.dll_hintnames - IMAGEBASE
+    at IMAGE_IMPORT_DESCRIPTOR.Name1,              dd msvcrt.dll - IMAGEBASE
+    at IMAGE_IMPORT_DESCRIPTOR.FirstThunk,         dd msvcrt.dll_iat - IMAGEBASE
+iend
+istruc IMAGE_IMPORT_DESCRIPTOR
+iend
+_d
+
+kernel32.dll_hintnames dd hnExitProcess - IMAGEBASE, 0
+msvcrt.dll_hintnames   dd hnprintf - IMAGEBASE, 0
+_d
+
+hnExitProcess db 0,0, 'ExitProcess', 0
+hnprintf      db 0,0, 'printf', 0
+_d
+
+kernel32.dll_iat:
+__imp__ExitProcess dd hnExitProcess - IMAGEBASE
+    dd 0
+
+msvcrt.dll_iat:
+__imp__printf dd hnprintf - IMAGEBASE
+    dd 0
+_d
+
+kernel32.dll db 'kernel32.dll', 0
+msvcrt.dll db 'msvcrt.dll', 0
+_d
+
+align FILEALIGN, db 0
