f02678dd0596fba571ea090fd1982c5f4b555b05
Ange Albertini <ange.albertini@gmail.com>
2013-08-15 05:11:34 +0000

    moved 'string in header' to its own PE
    git-svn-id: http://corkami.googlecode.com/svn/trunk@1712 0cc4791d-d43c-0410-a93a-b1ce904c0fb3

--- hdrdata.asm
+++ hdrdata.asm
@@ -1,4 +1,4 @@
-; a 'normal' PE - fishy, I know :p
+; a PE with data between header and first section
 
 ; Ange Albertini, BSD LICENCE 2009-2013
 
@@ -57,18 +57,26 @@ iend
 NUMBEROFSECTIONS equ ($ - SectionHeader) / IMAGE_SECTION_HEADER_size
 SIZEOFHEADERS equ $ - IMAGEBASE
 
+db "this will not be in memory, as it's between the declared header and before the first section"
+
 section progbits vstart=IMAGEBASE + SECTIONALIGN align=FILEALIGN
 
 EntryPoint:
+    mov eax, SIZEOFHEADERS + IMAGEBASE
+    mov ebx, dword [eax]
+    cmp ebx, 'this'
+    jz error_
+_
     push Msg
     call [__imp__printf]
     add esp, 1 * 4
 _
+error_:
     push 0
     call [__imp__ExitProcess]
 _c
 
-Msg db " * a standard PE (imports, standard alignments)", 0ah, 0
+Msg db " * a PE with data between header and first section", 0ah, 0
 _d
 
 Import_Descriptor:
