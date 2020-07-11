e2f437021cb274cdb8b41edd8f69c682da416f59
Ange <ange.albertini@gmail.com>
2020-07-11 17:53:18 +0200

    a few more signatures, better macros

--- polymocks/README.md
+++ polymocks/README.md
@@ -1,3 +1,58 @@
 # Polymocks
 
 Polymocks are files pretending to be polyglots but are actually invalid.
+
+## multi
+
+Output of `file --keep-going`:
+
+```
+multi: Windows Program Information File for \030(o\001
+- MAR Area Detector Image,
+- Linux kernel x86 boot executable RW-rootFS,
+- ReiserFS V3.6
+- Files-11 On-Disk Structure (ODS-52); volume label is '            '
+- DOS/MBR boot sector
+- Game Boy ROM image (Rev.00) [ROM ONLY], ROM: 256Kbit
+- Plot84 plotting file
+-  DOS/MBR boot sector
+- DOSFONT2 encrypted font data
+- Kodak Photo CD image pack file , landscape mode
+- SymbOS executable v., name: HNRO0\334\247\304\375]\034\236\243
+- ISO 9660 CD-ROM filesystem data (raw 2352 byte sectors)
+- High Sierra CD-ROM filesystem data
+- Nero CD image at 0x4B000 ISO 9660 CD-ROM filesystem data
+- Old EZD Electron Density Map
+- Apple File System (APFS), blocksize 24061976
+- Zoo archive data, modify: v78.88+
+- Symbian installation file
+- 4-channel Fasttracker module sound data Title: "MZ`\352\210\360'\315!"
+- Scream Tracker Sample adlib drum mono 8bit unpacked
+- Poly Tracker PTM Module Title: "MZ`\352\210\360'\315!"
+- SNDH Atari ST music
+- SoundFX Module sound file
+- D64 Image
+- Nintendo Wii disc image: "NXSB\030(o\001" (MZ`\35, Rev.205)
+- Nintendo 3DS File Archive (CFA) (v0, 0.0.0)
+- Unix Fast File system [v1] (little-endian), last mounted on , last written at Thu Jan 01 00:00:00 1970, clean flag 0, number of blocks 0, number of data blocks 0, number of cylinder groups 0, block size 0, fragment size 0, minimum percentage of free blocks 0, rotational delay 0ms, disk rotational speed 0rps, TIME optimization
+- Unix Fast File system [v2] (little-endian) last mounted on , last written at Thu Jan 01 01:00:00 1970, clean flag 0, readonly flag 0, number of blocks 0, number of data blocks 0, number of cylinder groups 0, block size 0, fragment size 0, average file size 0, average number of files in dir 0, pending blocks to free 0, pending inodes to free 0, system-wide uuid 0, minimum percentage of free blocks 0, TIME optimization
+- Unix Fast File system [v2] (little-endian) last mounted on , last written at Thu Jan 01 01:00:00 1970, clean flag 0, readonly flag 0, number of blocks 0, number of data blocks 0, number of cylinder groups 0, block size 0, fragment size 1934189906, average file size 0, average number of files in dir 0, pending blocks to free 0, pending inodes to free 0, system-wide uuid 0, minimum percentage of free blocks 115, TIME optimization
+- F2FS filesystem, UUID=00000000-0000-0000-0000-000000000000, volume name ""
+- ISO 9660 CD-ROM filesystem data (DOS/MBR boot sector)
+- DICOM medical imaging data
+- Linux kernel ARM boot executable zImage (little-endian)
+- CCP4 Electron Density Map
+- Ultrix core file from 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+- VirtualBox Disk Image (MZ`\352\210\360'\315!), 5715999566798081280 bytes
+- MS Compress archive data
+- AMUSIC Adlib Tracker
+-  MS-DOS executable, MZ for MS-DOS
+-  COM executable for DOS
+- JPEG 2000 image
+- ARJ archive data
+- unicos (cray) executable
+- IBM OS/400 save file data
+-  (Lepton 3.x), scale 0-0, spot sensor temperature 0.000000, unit celsius, color scheme 0, calibration: offset 0.000000, slope 0.000000
+-  (Lepton 2.x), scale 0-0, spot sensor temperature 0.000000, unit celsius, color scheme 0, calibration: offset 0.000000, slope 0.000000
+- data
+```
--- polymocks/multi.asm
+++ polymocks/multi.asm
@@ -15,16 +15,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Macros
 
+C equ 0
 ; hack to fill space until offset %1
 %macro _at 1
-  times (%1 - ($-$$)) db 0
+  times (%1 - ($-$$)) db (C & 0xff) + 0
+;  %assign C C+1
 %endmacro
 
 
-; puts %2 at offset %1
+; put %2 at offset %1
 %macro _ats 2
   _at %1
   %2
+%endmacro
+
+
+; extra signatures at variable offsets
+%macro _ex 1
+  %1
 %endmacro
 
 
@@ -38,16 +46,36 @@ _ats 0, {db "MZ"}       ; DOS executable
 
 
 _ats 2, {db 060h, 0EAh} ; ARJ
+;ats 2, {db "-LH1-"},   ; LHA archive (c64)
 
 
-_ats 4, {db 088h, 0F0h, 39} ; MS Compress
+_ats 4, {db "jP"}           ; JPEG 2000
+;ats 4, {db 088h, 0F0h, 39} ; MS Compress ; requires extra stuff
+;ats 4, {db "ftyp"}         ; ISO MEDIA
+;ats 4, {db "RED1"}         ; REDCode video
+;ats 4, {db "pipe"}         ; Clipper instruction trace
+;ats 4, {db "prof"}         ; Clipper instruction profile
+;ats 4, {db " A~"}          ; Mathematica .ml
+;ats 4, {db "PK", 1, 2}     ; Mozilla archive
+;ats 4, {dd 0xfb4a}         ; QDos executable
 
 
-_ats 7, {db 0CDh, 021h} ; fake INT 21h call to pretend it's a COM executable
+_ats 6, {dw 0x0701}              ; unicos (cray) executable
+;ats 6, {db "%!FontType1"}       ; PostScript Type 1 font program data
+;ats 6, {db "%!PS-AdobeFont-1."} ; PostScript Type 1 font program data
+
+;ats 7, {db "**ACE**"}           ; ACE archive
+;ats 7, {dd 0x00454741}          ; DOS code page font data
+;ats 7, {dd 0x00564944}          ; DOS code page font data (from Linux?)
 
 
-;ats 12, {db "BB02"} ; Bacula volume
-_ats 12, {db "SNDH"} ; SNDH Atari ST Music
+_ats 8, {dd 0x10000419}          ; Symbian installation file
+;ats 9, {db "PSUR"},             ; ARC archive (c64)
+
+
+_ats 12, {db "SNDH"}     ; SNDH Atari ST Music
+;ats 12, {db "BB02"}     ; Bacula volume
+;ats 12, {dd 0x00040988} ; Berkeley DB
 
 
 _ats 16, {db "NRO0"} ; Nintendo Switch executable
@@ -57,74 +85,108 @@ _ats 20, {dd 0xfdc4a7dc} ; Zoo archive
 
 
 _ats 24, {db 0x5D,0x1C,0x9E,0xA3} ; Nintendo Wii disc image
-;ats 26, {db "sfArk"}  ; compressed SoundFont
 
+
+_ex {db "RE", 0x7e, 0x5e} ; Old Rar
+
+
+;ats 26, {db "sfArk"}             ; compressed SoundFont
+
+;ats 28, {db "make config"}       ; Linux make config build file (old)
 
 _ats 32, {db "NXSB"}   ; Apple File System
 
+;ats 34, {db "UPX!"}   ; FREE-DOS executable (COM), UPX compressed
+
+;ats 35, {db "UPX!"}   ; FREE-DOS executable (COM), UPX compressed
 
 ;ats 36, {db "acsp"} ; ICC profile magic - not enough for detection
 _ats 36, {dd 0x016f2818}  ; Linux kernel ARM boot executable zImage
 
 
+_ex {db "PK", 3,4} ; Zip LFH
+
+
 ;_ats 43, {db "SFDU_LABEL"} ; VICAR label file
-_ats 44, {db "PTMF"}       ; Protracker
-_ats 48, {db "SymExe"} ; SymbOs executable
+
+_ats 44, {db "PTMF"}       ; Poly Tracker Module File
+
+_ats 48, {db "SymExe"}     ; SymbOs executable
+
+
+_ex {db "7z", 0xBC, 0xAF, 0x27, 0x1c} ; 7-zip
 
 
 ;ats 56, {db "hpux"} ; netbsd ktrace, but requires extra conditions
 
 
-_ats 60, {db "SONG"}     ; SoundFX Module sound file
-;ats 60, {db "BOOKMOBI"} ; Mobipocket E-book
-_ats 64, {dd 0xbeda107f}  ; VirtualBox Disk Image
-;ats 65, {db "PNTGMPNT"} ; MacPaint Image data
+_ats 60, {db "SONG"}           ; SoundFX Module sound file
+;ats 60, {db "BOOKMOBI"}       ; Mobipocket E-book
+;ats 60, {db "W Collis", 0, 0} ; COM executable for MS-DOS, Compack compressed
+
+_ats 64, {dd 0xbeda107f}       ; VirtualBox Disk Image
+
+;ats 65, {db "PNTGMPNT"}       ; MacPaint Image data
+
+
+_ats 70, {db 0CDh, 021h} ; fake INT 21h call to pretend it's a COM executable - also scanned on offsets 2, 4, 5, 13, 18, 23, 30, 70
+
+
+_ex {db "PK", 1,2} ; Zip CD
 
 
 ;_ats 73, {db "%%%  "} ; BibTeX
 _ats 76, {db "SCRS"}      ; Scream Tracker Sample
 
 
+_ex {db "Rar!", 0x1A, 7, 1, 0} ; Rar v5
+
+
+_ex {db "LRZI"} ; LRZIP
+
+
 _ats 92, {db "PLOT%%84"}  ; Plot84 plotting file
+
+
+_ex {db "Rar!", 0x1A, 7, 0} ; Rar v4
 
 
 _ats 109, {db "MAP ("}   ; EZD Electron Density Map
 
 
+_ex {db 0xFD, "7zXZ", 0} ; XZ
+_ex {dd 0x184d2204} ; LZ4
+_ex {dd 0x184c2103} ; LZ4
+
+
 _ats 128, {db "DICM"} ; DICOM
 
 
-;Rar
-db "RE", 0x7e, 0x5e
-db "Rar!", 0x17, 7, 0
-db "Rar!", 0x17, 7, 1, 0
+_ex {db "%PDF-1.4 obj stream endstream endobj xref trailer startxref"} ; PDF
 
 
 _ats 208, {db "MAP "} ; CCP4 Electron Density Map
 
 
+;ats 252, {db "Must have DOS version"} ; DR-DOS executable (COM)
+
+
 _ats 256, {db "NCCH"} ; Nintendo 3DS archive
+
+
 ;ats 257, {db "ustar"} ; TAR
+
+
 _ats 260, {db 0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B} ; Game Boy ROM image
 
 
 _ats 369, {db "MICROSOFT PIFEX", 0} ; Windows Program Information File
+
+
 ;ats 384, {db "LockStream"}         ; LockStream Embedded file
 
 
-db "7z", 0xBC, 0xAF, 0x27, 0x1C ; 7-zip
-
-
-db 0xFD, "7zXZ", 0 ; XZ
-
-
-db "LRZI" ; LRZIP
-
-
-; LZ4
-dd 0x184d2204
-dd 0x184c2103
-dd 0x184c2102
+_ex {dd 0x184c2102} ; LZ4
 
 
 _ats 510, {db 055h, 0AAh} ; Master Boot Record
@@ -133,17 +195,11 @@ _ats 510, {db 055h, 0AAh} ; Master Boot Record
 _ats 514, {db "HdrS"} ; Linux RW-rootFS
 
 
-db "%PDF-1.4 obj stream endstream endobj xref trailer startxref" ; PDF
-
-; ZIP structures
-db "PK", 3,4
-db "PK", 1,2
-db "PK", 5,6
-
 _ats 596, {db "X", 0DFh, 0FFh, 0FFh} ; Ultrix core file
 
 
-db "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H", "*" ; EICAR
+_ex {db "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H", "*"} ; EICAR
+
 
 _ats 1008, {db "DECFILE11"} ; Files-11 On-Disk Structure
 
@@ -198,3 +254,5 @@ _ats 91392, {dd 0x00410112} ; D64 Image
 
 
 _ats 339969, {db "CD001"} ; Nero ISO
+
+_ex {db "PK", 5,6} ; ZIP EoCD
